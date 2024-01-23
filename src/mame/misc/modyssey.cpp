d035e819aaff92b074d959b6b8d5c17d41563f08
stonedDiscord <Tukz@gmx.de>
2024-01-23 23:10:14 +0100

    add pots

--- src/mame/misc/modyssey.cpp
+++ src/mame/misc/modyssey.cpp
@@ -86,17 +86,40 @@ private:
 };
 
 static INPUT_PORTS_START( modyssey )
-	PORT_START( "PADDLE0" ) /* fake input port for player 1 paddle */
-	//PORT_BIT( 0x01, 0x10, IPT_PADDLE ) PORT_SENSITIVITY(2) PORT_KEYDELTA(100) PORT_CENTERDELTA(0)   NETLIST_ANALOG_PORT_CHANGED("maincpu", "p1hor")
-	//PORT_BIT( 0x01, 0x20, IPT_PADDLE ) PORT_SENSITIVITY(2) PORT_KEYDELTA(100) PORT_CENTERDELTA(0)   NETLIST_ANALOG_PORT_CHANGED("maincpu", "p1ver")
-	//PORT_BIT( 0x01, 0x30, IPT_PADDLE ) PORT_SENSITIVITY(2) PORT_KEYDELTA(100) PORT_CENTERDELTA(0)   NETLIST_ANALOG_PORT_CHANGED("maincpu", "p1eng")
+	PORT_START( "PADDLE0H" ) /* fake input port for player 1 paddle */
+	PORT_BIT( 0xff, 0x00, IPT_PADDLE ) PORT_SENSITIVITY(2) PORT_KEYDELTA(100) PORT_CENTERDELTA(0)   NETLIST_ANALOG_PORT_CHANGED("maincpu", "p1hor")
+	PORT_START( "PADDLE0V" )
+	PORT_BIT( 0xff, 0x00, IPT_PADDLE ) PORT_SENSITIVITY(2) PORT_KEYDELTA(100) PORT_CENTERDELTA(0)   NETLIST_ANALOG_PORT_CHANGED("maincpu", "p1ver")
+	PORT_START( "PADDLE0E" )
+	PORT_BIT( 0xff, 0x00, IPT_PADDLE ) PORT_SENSITIVITY(2) PORT_KEYDELTA(100) PORT_CENTERDELTA(0)   NETLIST_ANALOG_PORT_CHANGED("maincpu", "p1eng")
 	//PORT_BIT( 0x01, IP_ACTIVE_HIGH, IPT_START1 )     NETLIST_LOGIC_PORT_CHANGED("maincpu", "SW4_1")
 
-	PORT_START( "PADDLE1" ) /* fake input port for player 2 paddle */
-	//PORT_BIT( 0x01, 0x10, IPT_PADDLE ) PORT_SENSITIVITY(2) PORT_KEYDELTA(100) PORT_CENTERDELTA(0)   NETLIST_ANALOG_PORT_CHANGED("maincpu", "p2hor")
-	//PORT_BIT( 0x01, 0x20, IPT_PADDLE ) PORT_SENSITIVITY(2) PORT_KEYDELTA(100) PORT_CENTERDELTA(0)   NETLIST_ANALOG_PORT_CHANGED("maincpu", "p2ver")
-	//PORT_BIT( 0x01, 0x30, IPT_PADDLE ) PORT_SENSITIVITY(2) PORT_KEYDELTA(100) PORT_CENTERDELTA(0)   NETLIST_ANALOG_PORT_CHANGED("maincpu", "p2eng")
+	PORT_START( "PADDLE1H" ) /* fake input port for player 2 paddle */
+	PORT_BIT( 0xff, 0x00, IPT_PADDLE ) PORT_SENSITIVITY(2) PORT_KEYDELTA(100) PORT_CENTERDELTA(0) PORT_PLAYER(2) NETLIST_ANALOG_PORT_CHANGED("maincpu", "p2hor")
+	PORT_START( "PADDLE1V" )
+	PORT_BIT( 0xff, 0x00, IPT_PADDLE ) PORT_SENSITIVITY(2) PORT_KEYDELTA(100) PORT_CENTERDELTA(0) PORT_PLAYER(2)  NETLIST_ANALOG_PORT_CHANGED("maincpu", "p2ver")
+	PORT_START( "PADDLE1E" )
+	PORT_BIT( 0xff, 0x00, IPT_PADDLE ) PORT_SENSITIVITY(2) PORT_KEYDELTA(100) PORT_CENTERDELTA(0) PORT_PLAYER(2)  NETLIST_ANALOG_PORT_CHANGED("maincpu", "p2eng")
 	//PORT_BIT( 0x01, IP_ACTIVE_HIGH, IPT_START2 )     NETLIST_LOGIC_PORT_CHANGED("maincpu", "SW3_1")
+
+	PORT_START("POT_3")
+	PORT_ADJUSTER( 50, "Pot: Ball Speed" )  NETLIST_ANALOG_PORT_CHANGED("maincpu", "ballspeed")
+	PORT_START("POT_4")
+	PORT_ADJUSTER( 50, "Pot: Ball Height" )  NETLIST_ANALOG_PORT_CHANGED("maincpu", "ballheight")
+	PORT_START("POT_6")
+	PORT_ADJUSTER( 50, "Pot: Ball Width" )  NETLIST_ANALOG_PORT_CHANGED("maincpu", "ballwidth")
+	PORT_START("POT_12")
+	PORT_ADJUSTER( 50, "Pot: Wall Center Adjustment" )  NETLIST_ANALOG_PORT_CHANGED("maincpu", "wallcentadj")
+	PORT_START("POT_17")
+	PORT_ADJUSTER( 50, "Pot: Wall Width" )  NETLIST_ANALOG_PORT_CHANGED("maincpu", "wallwidth")
+	PORT_START("POT_26")
+	PORT_ADJUSTER( 50, "Pot: Player No.1 Width" )  NETLIST_ANALOG_PORT_CHANGED("maincpu", "p1width")
+	PORT_START("POT_28")
+	PORT_ADJUSTER( 50, "Pot: Player No.1 Height" )  NETLIST_ANALOG_PORT_CHANGED("maincpu", "p1height")
+	PORT_START("POT_31")
+	PORT_ADJUSTER( 50, "Pot: Player No.2 Width" )  NETLIST_ANALOG_PORT_CHANGED("maincpu", "p2width")
+	PORT_START("POT_32")
+	PORT_ADJUSTER( 50, "Pot: Player No.2 Height" )  NETLIST_ANALOG_PORT_CHANGED("maincpu", "p2height")
 
 INPUT_PORTS_END
 
@@ -105,11 +128,26 @@ void modyssey_state::modyssey(machine_config &config)
 	/* basic machine hardware */
 	NETLIST_CPU(config, "maincpu", netlist::config::DEFAULT_CLOCK()).set_source(NETLIST_NAME(modyssey));
 
-	//NETLIST_ANALOG_INPUT(config, "maincpu:p1hor", "RV4_61.R").set_mult_offset(1.0 / 100.0 * RES_K(50), RES_K(56) );
-	//NETLIST_ANALOG_INPUT(config, "maincpu:p1ver", "RV4_8.R").set_mult_offset(1.0 / 100.0 * RES_K(50), RES_K(56) );
+	NETLIST_ANALOG_INPUT(config, "maincpu:p1hor", "RV4_61.DIAL");
+	NETLIST_ANALOG_INPUT(config, "maincpu:p1ver", "RV4_8.DIAL");
+	NETLIST_ANALOG_INPUT(config, "maincpu:p1eng", "RV4_62.DIAL");
+
+	NETLIST_ANALOG_INPUT(config, "maincpu:p2hor", "RV3_61.DIAL");
+	NETLIST_ANALOG_INPUT(config, "maincpu:p2ver", "RV3_8.DIAL");
+	NETLIST_ANALOG_INPUT(config, "maincpu:p2eng", "RV3_62.DIAL");
 
 	//NETLIST_LOGIC_INPUT(config, "maincpu:p1reset", "SW4_1.POS", 0);
 	//NETLIST_LOGIC_INPUT(config, "maincpu:p2reset", "SW3_1.POS", 0);
+
+	NETLIST_ANALOG_INPUT(config, "maincpu:ballspeed", "RV3.DIAL");
+	NETLIST_ANALOG_INPUT(config, "maincpu:ballheight", "RV4.DIAL");
+	NETLIST_ANALOG_INPUT(config, "maincpu:ballwidth", "RV6.DIAL");
+	NETLIST_ANALOG_INPUT(config, "maincpu:wallcentadj", "RV12.DIAL");
+	NETLIST_ANALOG_INPUT(config, "maincpu:wallwidth", "RV17.DIAL");
+	NETLIST_ANALOG_INPUT(config, "maincpu:p1width", "RV26.DIAL");
+	NETLIST_ANALOG_INPUT(config, "maincpu:p1height", "RV28.DIAL");
+	NETLIST_ANALOG_INPUT(config, "maincpu:p2width", "RV31.DIAL");
+	NETLIST_ANALOG_INPUT(config, "maincpu:p2height", "RV32.DIAL");
 
 	NETLIST_ANALOG_OUTPUT(config, "maincpu:vid0", 0).set_params("videomix", "fixfreq", FUNC(fixedfreq_device::update_composite_monochrome));
 
