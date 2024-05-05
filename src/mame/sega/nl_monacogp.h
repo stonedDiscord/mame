7e24e1bca67d4cc8c2976d98d6d405a276b7e759
stonedDiscord <Tukz@gmx.de>
2024-05-06 00:45:51 +0200

    add netlist header file

---
+++ src/mame/sega/nl_monacogp.h
@@ -0,0 +1,10 @@
+// license:CC0-1.0
+// copyright-holders:stonedDiscord
+#ifndef MAME_SEGA_NL_MONACOGP_H
+#define MAME_SEGA_NL_MONACOGP_H
+
+#pragma once
+
+NETLIST_EXTERNAL(monacogp)
+
+#endif // MAME_SEGA_NL_MONACOGP_H
