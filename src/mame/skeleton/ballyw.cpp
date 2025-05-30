bfee452deb6c70566712aaa6efc4eecdc9989288
stonedDiscord <Tukz@gmx.de>
2025-05-30 21:52:41 +0200

    sizes

--- src/mame/skeleton/ballyw.cpp
+++ src/mame/skeleton/ballyw.cpp
@@ -23,7 +23,6 @@ public:
 	ballyw_state(const machine_config &mconfig, device_type type, const char *tag) :
 		driver_device(mconfig, type, tag),
 		m_maincpu(*this, "maincpu"),
-		m_nvram(*this, "nvram"),
         m_rtc(*this, "rtc"),
 		m_digits(*this, "digit%u", 0U)
 	{ }
@@ -35,7 +34,6 @@ private:
 
 	// devices
 	required_device<cpu_device> m_maincpu;
-	required_device<nvram_device> m_nvram;
     required_device<rtc72421_device> m_rtc;
 	output_finder<8> m_digits;
 };
@@ -43,15 +41,15 @@ private:
 
 void ballyw_state::mem_map(address_map &map)
 {
-	map(0x000000, 0x01ffff).rom();
+	map(0x000000, 0x0fffff).rom();
 }
 
 static INPUT_PORTS_START( ballyw )
 	PORT_START("IN0")
 	PORT_BIT( 0x0001, IP_ACTIVE_HIGH, IPT_GAMBLE_HIGH ) // Left
 	PORT_BIT( 0x0002, IP_ACTIVE_HIGH, IPT_START )
-	PORT_BIT( 0x0004, IP_ACTIVE_HIGH, IPT_SLOT_SPLIT )
-	PORT_BIT( 0x0008, IP_ACTIVE_HIGH, IPT_SLOT_STOP )
+	PORT_BIT( 0x0004, IP_ACTIVE_HIGH, IPT_GAMBLE_BET )
+	PORT_BIT( 0x0008, IP_ACTIVE_HIGH, IPT_SLOT_STOP1 )
 	PORT_BIT( 0x0010, IP_ACTIVE_HIGH, IPT_GAMBLE_LOW ) // Right
 INPUT_PORTS_END
 
@@ -60,24 +58,23 @@ void ballyw_state::ballyw(machine_config &config)
 {
 	M68000(config, m_maincpu, 10000000 ); //?
 	m_maincpu->set_addrmap(AS_PROGRAM, &ballyw_state::mem_map);
-	m_maincpu->set_addrmap(m68000_device::AS_CPU_SPACE, &ballyw_state::fc7_map);
 
     RTC72421(config, "rtc", XTAL(32'768)); // internal oscillator
 }
 
 ROM_START( gloriasl )
-	ROM_REGION( 0x20000, "maincpu", 0 )
-	ROM_LOAD16_BYTE( "123-S4.0_even_GLORIA_SL.ic15", 0x00000, 0x10000, CRC(1be58a11) SHA1(1a1b1f51c9c3f4bd16832a689ee90a5f8faac453) )
-	ROM_LOAD16_BYTE( "123-S4.0_odd_GLORIA_SL.ic10", 0x00001, 0x10000, CRC(06d7ee41) SHA1(8bf4b1ce16d9f021381b7dae5069ba8383b150cd) )
+	ROM_REGION( 0x100000, "maincpu", 0 )
+	ROM_LOAD16_BYTE( "123-S4.0_even_GLORIA_SL.ic15", 0x00001, 0x80000, CRC(1be58a11) SHA1(1a1b1f51c9c3f4bd16832a689ee90a5f8faac453) )
+	ROM_LOAD16_BYTE( "123-S4.0_odd_GLORIA_SL.ic10", 0x00000, 0x80000, CRC(06d7ee41) SHA1(8bf4b1ce16d9f021381b7dae5069ba8383b150cd) )
 ROM_END
 
 ROM_START( sunfun )
 	ROM_REGION( 0x100000, "maincpu", 0 )
-	ROM_LOAD16_BYTE( "sunfun_even.BIN", 0x00000, 0x80000, CRC(f03bdbef) SHA1(9dc08b684e03b489953c1867902b2164cd2a4f8d) )
-	ROM_LOAD16_BYTE( "sunfun_odd.BIN", 0x00001, 0x80000, CRC(5b52038a) SHA1(9dc08b684e03b489953c1867902b2164cd2a4f8d) )
+	ROM_LOAD16_BYTE( "sunfun_even.ic15", 0x00001, 0x80000, CRC(f03bdbef) SHA1(9dc08b684e03b489953c1867902b2164cd2a4f8d) )
+	ROM_LOAD16_BYTE( "sunfun_odd.ic10", 0x00000, 0x80000, CRC(5b52038a) SHA1(9dc08b684e03b489953c1867902b2164cd2a4f8d) )
 ROM_END
 
 } // anonymous namespace
 
-GAMEL(2003, gloriasl,        0, ballyw, ballyw, ballyw_state, empty_init, ROT0, "Bally Wulff", "Gloria SL",  MACHINE_NOT_WORKING | MACHINE_MECHANICAL | MACHINE_REQUIRES_ARTWORK, layout_ballyw )
-GAMEL(2003, sunfun,   gloriasl, ballyw, ballyw, ballyw_state, empty_init, ROT0, "Bally Wulff", "Sun Fun",    MACHINE_NOT_WORKING | MACHINE_MECHANICAL | MACHINE_REQUIRES_ARTWORK, layout_ballyw )
+GAMEL(2003, gloriasl,        0, ballyw, ballyw, ballyw_state, empty_init, ROT0, "Bally Wulff", "Gloria SL",  MACHINE_NOT_WORKING | MACHINE_MECHANICAL | MACHINE_REQUIRES_ARTWORK, layout_stellafr )
+GAMEL(2003, sunfun,   gloriasl, ballyw, ballyw, ballyw_state, empty_init, ROT0, "Bally Wulff", "Sun Fun",    MACHINE_NOT_WORKING | MACHINE_MECHANICAL | MACHINE_REQUIRES_ARTWORK, layout_stellafr )
