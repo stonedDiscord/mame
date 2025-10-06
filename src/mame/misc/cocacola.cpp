d7ba3d28abdaf9cc680e0822c0da14d4903305e4
stonedDiscord <Tukz@gmx.de>
2025-10-06 21:16:49 +0200

    dips

--- src/mame/misc/cocacola.cpp
+++ src/mame/misc/cocacola.cpp
@@ -13,8 +13,6 @@
 #include "emupal.h"
 #include "screen.h"
 
-//#include "cocacola.lh"
-
 namespace {
 
 class cocacola_state : public driver_device
@@ -87,37 +85,35 @@ static INPUT_PORTS_START( cocacola )
 	PORT_BIT( 0xc0, IP_ACTIVE_LOW, IPT_BUTTON7 )
 
 	PORT_START("DSW1")
-	PORT_DIPNAME( 0x01, 0x00, DEF_STR( Unknown ) )  PORT_DIPLOCATION("SW1:1")
-	PORT_DIPSETTING( 0x01, DEF_STR( Off ) )
-	PORT_DIPSETTING( 0x00, DEF_STR( On ) )
-	PORT_DIPNAME( 0x02, 0x02, DEF_STR( Unknown ) )    PORT_DIPLOCATION("SW1:2")
-	PORT_DIPSETTING( 0x02, DEF_STR( Off ) )
-	PORT_DIPSETTING( 0x00, DEF_STR( On ) )
-	PORT_DIPNAME( 0x04, 0x04, DEF_STR( Unknown ) )    PORT_DIPLOCATION("SW1:3")
-	PORT_DIPSETTING( 0x04, DEF_STR( Off ) )
-	PORT_DIPSETTING( 0x00, DEF_STR( On ) )
-	PORT_DIPNAME( 0x08, 0x08, DEF_STR( Unknown ) )    PORT_DIPLOCATION("SW1:4")
-	PORT_DIPSETTING( 0x08, DEF_STR( Off ) )
-	PORT_DIPSETTING( 0x00, DEF_STR( On ) )
-	PORT_DIPNAME( 0x10, 0x10, DEF_STR( Unknown ) )    PORT_DIPLOCATION("SW1:5")
-	PORT_DIPSETTING( 0x10, DEF_STR( Off ) )
-	PORT_DIPSETTING( 0x00, DEF_STR( On ) )
+	PORT_DIPNAME( 0x01, 0x00, "Light barrier" )  PORT_DIPLOCATION("SW1:1")
+	PORT_DIPSETTING( 0x00, DEF_STR( Off ) )
+	PORT_DIPSETTING( 0x01, DEF_STR( On ) )
+	PORT_DIPNAME( 0x02, 0x00, "Setup" )    PORT_DIPLOCATION("SW1:2")
+	PORT_DIPSETTING( 0x00, DEF_STR( Off ) )
+	PORT_DIPSETTING( 0x02, DEF_STR( On ) )
+	PORT_DIPNAME( 0x04, 0x00, "Test" )    PORT_DIPLOCATION("SW1:3")
+	PORT_DIPSETTING( 0x00, DEF_STR( Off ) )
+	PORT_DIPSETTING( 0x04, DEF_STR( On ) )
+	PORT_DIPNAME( 0x08, 0x00, "Empty lamps" )    PORT_DIPLOCATION("SW1:4")
+	PORT_DIPSETTING( 0x00, DEF_STR( Off ) )
+	PORT_DIPSETTING( 0x08, DEF_STR( On ) )
+	PORT_DIPNAME( 0x10, 0x00, "Clear" )    PORT_DIPLOCATION("SW1:5")
+	PORT_DIPSETTING( 0x00, DEF_STR( Off ) )
+	PORT_DIPSETTING( 0x10, DEF_STR( On ) )
 INPUT_PORTS_END
 
 void cocacola_state::cocacola(machine_config &config)
 {
-	m6802_cpu_device &maincpu(M6802(config, m_maincpu, XTAL(4'000'000)));
+	m6802_cpu_device &maincpu(M6802(config, m_maincpu, XTAL(4'000'000))); // TODO: verify clock
 	maincpu.set_ram_enable(false);
 	maincpu.set_addrmap(AS_PROGRAM, &cocacola_state::cocacola_map);
-	//config.set_default_layout(layout_cocacola);
 
-	/* Devices */
 	PIA6821(config, m_pia);
 
 	ACIA6850(config, m_acia0, 0);
 	ACIA6850(config, m_acia1, 0);
 
-	MC146818(config, m_rtc, 32.768_kHz_XTAL); // TODO: verify clock
+	MC146818(config, m_rtc, 32.768_kHz_XTAL);
 
 	screen_device &screen(SCREEN(config, "screen", SCREEN_TYPE_LCD));
 	screen.set_refresh_hz(50);
@@ -126,7 +122,7 @@ void cocacola_state::cocacola(machine_config &config)
 	screen.set_visarea(0, 16*6-1, 0, 16-1);
 	screen.set_palette("palette");
 
-	hd44780_device &hd44780(HD44780(config, m_lcd, 270'000)); // TODO: clock not measured, datasheet typical clock used
+	hd44780_device &hd44780(HD44780(config, m_lcd, 270'000));
 	hd44780.set_lcd_size(2, 16);
 	hd44780.set_pixel_update_cb(FUNC(cocacola_state::pixel_update));
 
