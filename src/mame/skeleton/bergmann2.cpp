0ea3f95888f6c9355b7d8393b3184925d162ec93
stonedDiscord <Tukz@gmx.de>
2026-02-23 12:29:21 +0100

    manufacturer and brand name

--- src/mame/skeleton/bergmann2.cpp
+++ src/mame/skeleton/bergmann2.cpp
@@ -467,7 +467,7 @@ ROM_END
 
 } // anonymous namespace
 
-GAMEL( 1983, croyal1,  0, bergmann2, bergmann2, bergmann2_state, empty_init, ROT0, "Crown Royal", "No.1",    MACHINE_NOT_WORKING, layout_croyal1 )
-GAMEL( 1984, corsar,   0, bergmann2, bergmann2, bergmann2_state, empty_init, ROT0, "Crown", "Corsar",        MACHINE_NOT_WORKING, layout_croyal1 )
-GAMEL( 1984, jubilees, 0, bergmann2, bergmann2, bergmann2_state, empty_init, ROT0, "Crown", "Jubilee Super", MACHINE_NOT_WORKING, layout_croyal1 )
-GAMEL( 1984, cwinner,  0, bergmann2, bergmann2, bergmann2_state, empty_init, ROT0, "Crown", "Winner",        MACHINE_NOT_WORKING, layout_croyal1 )
+GAMEL( 1983, croyal1,  0, bergmann2, bergmann2, bergmann2_state, empty_init, ROT0, "Bergmann Automaten", "Crown Royal No.1",    MACHINE_NOT_WORKING, layout_croyal1 )
+GAMEL( 1984, corsar,   0, bergmann2, bergmann2, bergmann2_state, empty_init, ROT0, "Bergmann Automaten", "Crown Corsar",        MACHINE_NOT_WORKING, layout_croyal1 )
+GAMEL( 1984, jubilees, 0, bergmann2, bergmann2, bergmann2_state, empty_init, ROT0, "Bergmann Automaten", "Crown Jubilee Super", MACHINE_NOT_WORKING, layout_croyal1 )
+GAMEL( 1984, cwinner,  0, bergmann2, bergmann2, bergmann2_state, empty_init, ROT0, "Bergmann Automaten", "Crown Winner",        MACHINE_NOT_WORKING, layout_croyal1 )
