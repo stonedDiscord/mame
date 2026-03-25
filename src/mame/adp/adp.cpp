23b48bec8da2b06005519793989d00f7d2509ddc
stonedDiscord <Tukz@gmx.de>
2026-03-25 18:10:27 +0100

    adp/adp: allow reading ramdac mask

--- src/mame/adp/adp.cpp
+++ src/mame/adp/adp.cpp
@@ -342,7 +342,7 @@ void adp_state::funland_mem(address_map &map)
 	map(0x800080, 0x800083).rw(m_acrtc, FUNC(hd63484_device::read16), FUNC(hd63484_device::write16));
 	map(0x800089, 0x800089).w("ramdac", FUNC(ramdac_device::index_w));
 	map(0x80008b, 0x80008b).w("ramdac", FUNC(ramdac_device::pal_w));
-	map(0x80008d, 0x80008d).w("ramdac", FUNC(ramdac_device::mask_w));
+	map(0x80008d, 0x80008d).rw("ramdac", FUNC(ramdac_device::mask_r), FUNC(ramdac_device::mask_w));
 	map(0x800100, 0x800101).portr(m_in0);
 	map(0x800140, 0x800143).rw("aysnd", FUNC(ay8910_device::data_r), FUNC(ay8910_device::address_data_w)).umask16(0x00ff); //18b too
 	map(0x800180, 0x80019f).rw(m_duart, FUNC(mc68681_device::read), FUNC(mc68681_device::write)).umask16(0x00ff);
