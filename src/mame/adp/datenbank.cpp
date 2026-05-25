4312ed5184e48aaa40573280f8b21a326d278979
stonedDiscord <Tukz@gmx.de>
2026-05-25 16:25:18 +0200

    1mb

--- src/mame/adp/datenbank.cpp
+++ src/mame/adp/datenbank.cpp
@@ -405,7 +405,8 @@ void datenbank_state::ay8910_portb_w(uint8_t data)
 
 void datenbank_state::mem_map(address_map &map)
 {
-	map(0x000000, 0x7fffff).ram().share("nvram");
+	map(0x000000, 0x0fffff).ram().share("nvram");
+	map(0x000000, 0x0003ff).rom().region("nvram",0x1100);
 	// controlled by U17 74HC138
 	map(0x800001, 0x800001).w(m_dac, FUNC(dac_byte_interface::data_w)); // Y0
 	// Y1 device on cpu board
@@ -416,6 +417,7 @@ void datenbank_state::mem_map(address_map &map)
 	map(0x800143, 0x800143).w("aysnd", FUNC(ay8910_device::data_w)); // Y5
 	map(0x800180, 0x80019f).rw(m_duart, FUNC(mc68681_device::read), FUNC(mc68681_device::write)).umask16(0x00ff); // Y6
 	// Y7 NC
+	map(0xfff000, 0xffffff).ram();
 }
 
 void datenbank_state::fc7_map(address_map &map)
@@ -469,24 +471,24 @@ void datenbank_state::showdownec1(machine_config &config)
 }
 
 ROM_START( showdownec1 )
-    ROM_REGION16_BE( 0x80000, "nvram", 0 )
+    ROM_REGION16_BE( 0x100000, "nvram", 0 )
 	ROM_LOAD( "loader_rote.bin", 0x00400, 0x00bc0, CRC(6f6a4f49) SHA1(fd2ec05d52aeea588edcf6e22c7f6bc6dfb8d0d1) )
-	ROM_LOAD( "eeprom_1mb_at90s1200.bin", 0x00fc0, 0x00040, CRC(900fa426) SHA1(386b562b827665273fbc251f7c212651fff8c315) )
+	ROM_LOAD( "eeprom_512kb_at90s1200.bin", 0x00fc0, 0x00040, CRC(900fa426) SHA1(386b562b827665273fbc251f7c212651fff8c315) )
 	ROM_LOAD( "showdown_ec1_decrypted.bin", 0x01000, 0x50c04, CRC(39f72304) SHA1(a4c383f83a8c455c59fd16af3608119b1fab4f5b) )
 ROM_END
 
 ROM_START( brisant )
-    ROM_REGION16_BE( 0x80000, "nvram", 0 )
+    ROM_REGION16_BE( 0x100000, "nvram", 0 )
 	ROM_LOAD( "loader_rote.bin", 0x00400, 0x00bc0, CRC(6f6a4f49) SHA1(fd2ec05d52aeea588edcf6e22c7f6bc6dfb8d0d1) )
-	ROM_LOAD( "eeprom_1mb_at90s1200.bin", 0x00fc0, 0x00040, CRC(900fa426) SHA1(386b562b827665273fbc251f7c212651fff8c315) )
-	ROM_LOAD( "brisant_ec1.xc.dec.bin",     0x000000, 0x052404, CRC(83b81f46) SHA1(5c83bf81f285cac8a918dc5fdcb270a57588a1b9) )
+	ROM_LOAD( "eeprom_512kb_at90s1200.bin", 0x00fc0, 0x00040, CRC(900fa426) SHA1(386b562b827665273fbc251f7c212651fff8c315) )
+	ROM_LOAD( "brisant_ec1.xc.dec.bin",     0x001000, 0x052404, CRC(83b81f46) SHA1(5c83bf81f285cac8a918dc5fdcb270a57588a1b9) )
 ROM_END
 
 ROM_START( siriusje )
-    ROM_REGION16_BE( 0x80000, "nvram", 0 )
+    ROM_REGION16_BE( 0x100000, "nvram", 0 )
 	ROM_LOAD( "loader_rote.bin", 0x00400, 0x00bc0, CRC(6f6a4f49) SHA1(fd2ec05d52aeea588edcf6e22c7f6bc6dfb8d0d1) )
-	ROM_LOAD( "eeprom_1mb_at90s1200.bin", 0x00fc0, 0x00040, CRC(900fa426) SHA1(386b562b827665273fbc251f7c212651fff8c315) )
-	ROM_LOAD( "sirius_jackpot_ext_c2.xc.dec.bin",   0x000000, 0x018804, CRC(010a61e5) SHA1(1117ad3d97f0a08c4111ac52e9bf4edea0b0bac5) )
+	ROM_LOAD( "eeprom_512kb_at90s1200.bin", 0x00fc0, 0x00040, CRC(900fa426) SHA1(386b562b827665273fbc251f7c212651fff8c315) )
+	ROM_LOAD( "sirius_jackpot_ext_c2.xc.dec.bin",   0x001000, 0x018804, CRC(010a61e5) SHA1(1117ad3d97f0a08c4111ac52e9bf4edea0b0bac5) )
 ROM_END
 
 } // anonymous namespace
