1cba42f07b352bd42fd8f9c212805f01c0704380
stonedDiscord <Tukz@gmx.de>
2026-06-18 18:48:26 +0200

    adp/stella8085: remove dumb rst65 handler

--- src/mame/adp/stella8085.cpp
+++ src/mame/adp/stella8085.cpp
@@ -134,7 +134,6 @@ private:
 	void kbd_sl_w(uint8_t data);
 	void kbd_bd_w(uint8_t data);
 	void disp_w(uint8_t data);
-	void rst65_w(uint8_t state);
 	void output_digit(uint8_t i, uint8_t data);
 
 	void io00(uint8_t data) ATTR_COLD;
@@ -464,11 +463,6 @@ TIMER_CALLBACK_MEMBER(stella8085_state::sound_stop)
 	m_beep->set_state(0);
 }
 
-void stella8085_state::rst65_w(uint8_t state)
-{
-	m_maincpu->set_input_line(I8085_RST65_LINE, state ? ASSERT_LINE : CLEAR_LINE);
-}
-
 void stella8085_state::io00(uint8_t data)
 {
 	//old boards
@@ -773,7 +767,7 @@ void stella8085_state::dicemstr(machine_config &config)
 	m_kdc->out_sl_callback().set(FUNC(stella8085_state::kbd_sl_w));
 	m_kdc->out_disp_callback().set(FUNC(stella8085_state::disp_w));
 	m_kdc->in_rl_callback().set(FUNC(stella8085_state::kbd_rl_r));
-	m_kdc->out_irq_callback().set(FUNC(stella8085_state::rst65_w));
+	m_kdc->out_irq_callback().set_inputline(m_maincpu, I8085_RST65_LINE);
 
 	RTC62421(config, "rtc", 32.768_kHz_XTAL);
 
@@ -818,7 +812,7 @@ void stella8085_state::doppelpot(machine_config &config)
 	m_kdc->out_bd_callback().set(FUNC(stella8085_state::kbd_bd_w));
 	m_kdc->out_disp_callback().set(FUNC(stella8085_state::disp_w));
 	m_kdc->in_rl_callback().set(FUNC(stella8085_state::kbd_rl_r));
-	m_kdc->out_irq_callback().set(FUNC(stella8085_state::rst65_w));
+	m_kdc->out_irq_callback().set_inputline(m_maincpu, I8085_RST65_LINE);
 
 	config.set_default_layout(layout_adpservice);
 
