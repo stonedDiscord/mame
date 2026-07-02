1ae75867f179579c0a91ae3c2ebfc4cbff41b8a1
stonedDiscord <Tukz@gmx.de>
2026-07-02 21:58:03 +0200

    ctc address

--- src/devices/bus/ddr/zre.cpp
+++ src/devices/bus/ddr/zre.cpp
@@ -1,6 +1,8 @@
 #include "emu.h"
 #include "k1520.h"
 
+#include "zre.h"
+
 DEFINE_DEVICE_TYPE(K1520_ZRE, k1520_zre_k2521_device, "k1520_zre", "K1520 K2521 ZRE Board")
 DEFINE_DEVICE_TYPE(K1520_ZRE_8786, k1520_zre_8786_device, "k1520_zre_8786", "K1520 045-8786 ZRE Board")
 
@@ -16,7 +18,7 @@ k1520_zre_k2521_device::k1520_zre_k2521_device(machine_config const &mconfig, de
 	device_k1520_card_interface(mconfig, *this),
 	m_maincpu(*this, "maincpu"),
 	m_ctc(*this, "ctc"),
-	m_sio(*this, "sio"),
+	m_pio(*this, "pio"),
 	m_rom(*this, "rom"),
 	m_ram{ }
 {
@@ -28,14 +30,9 @@ void k1520_zre_k2521_device::device_add_mconfig(machine_config &config)
 	m_maincpu->set_addrmap(AS_PROGRAM, &k1520_zre_k2521_device::mem_map);
 	m_maincpu->set_addrmap(AS_IO, &k1520_zre_k2521_device::io_map);
 
-	z80ctc_device &ctc(Z80CTC(config, m_ctc, XTAL(9'830'400) / 4));
-	ctc.set_clk<0>(XTAL(9'830'400) / 4);
-	ctc.set_clk<1>(XTAL(9'830'400) / 4);
-	ctc.set_clk<2>(XTAL(9'830'400) / 4);
-	ctc.set_clk<3>(XTAL(9'830'400) / 4);
-	ctc.zc_callback<0>().set(m_sio, FUNC(z80sio_device::rxtxcb_w));
+	Z80CTC(config, m_ctc, XTAL(9'830'400) / 4);
 
-	Z80SIO(config, m_sio, XTAL(9'830'400) / 4);
+	Z80PIO(config, m_pio, XTAL(9'830'400) / 4);
 }
 
 void k1520_zre_k2521_device::device_start()
@@ -62,8 +59,8 @@ void k1520_zre_k2521_device::io_map(address_map &map)
 {
 	map.global_mask(0xff);
 	map(0x00, 0xff).rw(FUNC(k1520_zre_k2521_device::bus_io_r), FUNC(k1520_zre_k2521_device::bus_io_w));
-	map(0x50, 0x53).rw(m_sio, FUNC(z80sio_device::ba_cd_r), FUNC(z80sio_device::ba_cd_w));
-	map(0xe0, 0xe7).rw(m_ctc, FUNC(z80ctc_device::read), FUNC(z80ctc_device::write));
+	map(0x80, 0x83).rw(m_ctc, FUNC(z80ctc_device::read), FUNC(z80ctc_device::write));
+	map(0x84, 0x87).rw(m_pio, FUNC(z80pio_device::read), FUNC(z80pio_device::write));
 }
 
 u8 k1520_zre_k2521_device::bus_memory_r(offs_t offset)
@@ -110,22 +107,4 @@ bool k1520_zre_k2521_device::memory_w(offs_t offset, u8 data)
 
 	m_ram[offset & 0x3ff] = data;
 	return true;
-}
-
-
-k1520_zre_8786_device::k1520_zre_8786_device(machine_config const &mconfig, char const *tag, device_t *owner, u32 clock) :
-	k1520_zre_k2521_device(mconfig, K1520_ZRE_8786, tag, owner, clock),
-	m_rom(*this, "rom")
-{
-}
-
-bool k1520_zre_8786_device::memory_r(offs_t offset, u8 &data)
-{
-	if (offset <= 0x4fff)
-	{
-		data = m_rom[offset];
-		return true;
-	}
-
-	return k1520_zre_k2521_device::memory_r(offset, data);
 }
