eda5b261ac016bd7c035ef525ccc1f6c1345945d
stonedDiscord <Tukz@gmx.de>
2026-07-28 17:28:09 +0200

    premade nvram

--- src/mame/adp/manohman.cpp
+++ src/mame/adp/manohman.cpp
@@ -502,12 +502,18 @@ ROM_START( backgamn )
 	ROM_REGION( 0x20000, "maincpu", 0 )
 	ROM_LOAD16_BYTE( "b_f2_i.bin",  0x00000, 0x10000, CRC(9e42937c) SHA1(85d462a560b85b03ee9d341e18815b7c396118ac) )
 	ROM_LOAD16_BYTE( "b_f2_ii.bin", 0x00001, 0x10000, CRC(8e0ee50c) SHA1(2a05c337db1131b873646aa4109593636ebaa356) )
+
+  ROM_REGION( 0x4000, "nvram", 0 ) // crafted by running 342e
+	ROM_LOAD( "backgammon_wand_nvram.bin", 0x0000, 0x4000, CRC(f273ded5) SHA1(eb7702235febce95efa0ed5e6fea5daf2c447853) )
 ROM_END
 
 ROM_START( backgamnw )
 	ROM_REGION( 0x20000, "maincpu", 0 )
 	ROM_LOAD16_BYTE( "backgammon_wand_f2_i.u19",  0x00001, 0x10000, CRC(45e280d2) SHA1(65dff9d4884af2d37138f79d71bddd223433b13c) )
 	ROM_LOAD16_BYTE( "backgammon_wand_f2_ii.u20", 0x00000, 0x10000, CRC(ccc536c3) SHA1(82d9243d6f6e05d4ce83d1080c6c038852d2398c) )
+
+	ROM_REGION( 0x4000, "nvram", 0 ) // crafted by running 342e
+	ROM_LOAD( "backgammon_wand_nvram.bin", 0x0000, 0x4000, CRC(f273ded5) SHA1(eb7702235febce95efa0ed5e6fea5daf2c447853) )
 ROM_END
 
 } // anonymous namespace
